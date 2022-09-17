fd1c16eb5b947a7f661320d1520b8b5cc40ad691
OctopusLian <zoctopus@qq.com>
2022-09-17 14:18:47 +0800

    新增路由

--- app.py
+++ app.py
@@ -4,5 +4,29 @@ app = Flask(__name__, static_folder='assets')
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def index():
     return render_template('index.html')
+
+@app.route('/follow')
+def follow():
+    return render_template('follow.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/write')
+def write():
+    return render_template('write.html')
+
+@app.route('/mine')
+def mine():
+    return render_template('mine.html')
+
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
